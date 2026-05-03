3703ce177570357c7d6e807e6293a13b54a6b722
nikhilverma-99 <nikhil.kops@gmail.com>
2026-05-03 15:18:40 +0530

    chore: trigger container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { mount as MarketingApp } from "marketing/MarketingApp";
-import { useEffect } from "react";
 import { useRef } from "react";
+import { useEffect } from "react";
 
 export default () => {
   const ref = useRef(null);
